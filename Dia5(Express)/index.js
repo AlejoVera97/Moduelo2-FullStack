d841482db11eb362a50bcc97f08a883ac739c3e9
Alejo Vera Laffratta <alejoveralaffratta21@gmail.com>
2025-01-28 07:55:17 -0300

    Dia 6

---
+++ Dia5(Express)/index.js
@@ -0,0 +1,22 @@
+const express = requiere("express");
+
+const servidor = express();
+
+
+
+servidor.use((peticion,respuesta,siguiente)=>{
+console.log("cualquier proceso, bla bla bla")
+siguiente();
+});
+servidor.use("/algo",(peticion,respuesta)=>{
+    respuesta.send("middleware para /algo");
+});
+servidor.use("/algo",(peticion,respuesta)=>{
+    respuesta.send("middleware para /algo");
+});
+servidor.use((peticion,respuesta)=>{
+        respuesta.status(404);
+        respuesta.send("not found");
+});
+servidor.listen(3000);
+
