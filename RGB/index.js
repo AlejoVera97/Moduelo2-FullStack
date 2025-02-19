6f8075bd71939a922ea1ec8c0eebd5c770a332e3
Alejo Vera Laffratta <alejoveralaffratta21@gmail.com>
2025-02-19 07:57:01 -0300

    rbg2

--- RGB/index.js
+++ RGB/index.js
@@ -1,22 +1,54 @@
 import express from "express";
-import {leerColores,crearColor,borrarColor} from "./db.ks"
+import { leerColores, crearColor, borrarColor } from "./db.ks"
 
 const servidor = express();
 
+servidor.use(express.json());
+
 servidor.use(express.static("./estaticos"));
 
-servidor.get("/colores", async (peticion, respuesta) =>{
-    try{
-        let colores= await leerColores();
-
+servidor.get("/colores", async (peticion, respuesta) => {
+    try {
+        let colores = await leerColores();
         respuesta.json(colores);
+    }
+    catch (error) {
+        respuesta.status(500);
+        respuesta.json({ error: "error en el servidor" });
+    }
+});
+
+
+servidor.post("/crear-color", async (peticion, respuesta, siguiente) => {
+    let { r, b, g } = peticion.body;
+    let valido = true;
+    let exp = /^[0-9]{1,3$/;
+
+
+    [r, g, b].forEach(n => {
+        valido = valido && exp.text(n) && Number(n) <= 255;
+    });
+
+    if (valido) {
+
+        try {
+            let id = await crearColor({ r, g, b });
+            return respuesta.json({ id });
+
+        } catch (error) {
+            respuesta.status(500);
+            respuesta.json({ error: " error enla peticion" });
+        }
 
     }
-    catch(error){
-        respuesta.status(500);
-        
-        respuesta.json({error: "error en el servidor"});
-    }
+    siguiente(true);
+
+
+});
+
+servidor.use((error, peticion, respuesta, siguiente) => {
+    respuesta.status(400);
+    respuesta.json({ error: " error enla peticion" });
 });
 
 servidor.listen(4000);
