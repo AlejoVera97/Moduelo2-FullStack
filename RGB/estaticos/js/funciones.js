c64ac482f2a07e8869183f4e19132232f5f87117
Alejo Vera Laffratta <alejoveralaffratta21@gmail.com>
2025-02-20 07:34:50 -0300

    RGB3

--- RGB/estaticos/js/funciones.js
+++ RGB/estaticos/js/funciones.js
@@ -1,70 +1,88 @@
 const contenedor = document.querySelector("ul");
 const parrafoError = document.querySelector(".error")
-const formulario= document.querySelector("form");
-const input=document.querySelector(`form input[type="text"]`);
+const formulario = document.querySelector("form");
+const input = document.querySelector(`form input[type="text"]`);
 
 function li({ id, r, g, b }) {
     let item = document.createElement("li");
     let valor = [r, g, b].join(",");
     item.innerText = valor;
     item.style.backgroundColor = `rgb(${valor})`
-    return item;
-}
 
-fetch("/colores")
-    .then(respuesta => {
-        return new Promise((ok, ko) => {
-            if (respuesta.status == 500) {
-                return ko();
-            }
-            respuesta.json()
-                .then(colores => ok(colores))
-        });
 
-    })
-    .then(colores => {
-        colores.forEach(color => contenedor.appendChild(li(color)))
-            .catch(() => {
-                parrafoError.innerText = "ha ocurrido un error";
+    item.addEventListener("click", () => {
+        item.innerText = "borrando...";
+
+        fetch(`/borrar-color/${id}`, {
+            method: "DELETE"
+        })
+            .then(respuesta => {
+                if (respuesta.status == 204) {
+                    return item.remove();
+                }
+                item.innerText = valor;
+                parrafoError.innerText="ha ocurrido un error";
                 parrafoError.classList.add("visible");
-            })
+
+            });
+
+        
     });
 
-    formulario.addEventListener("submit",evento =>{
-        evento.preventDefault();
-        
-        parrafoError.classList.remove("visible");
-        if(/^([0-9]{1,3},){2}[0-9]{1,3}$/.test(input.value)){
-           let valores=input.value.split(",").map( n => Number(n));
-           let valido = true;
-           let i =0;
-           
-           while(valido && i < 3){
-            valido = valores[i]<= 255;
-            i++;
-           }
+fetch("/colores")
+            .then(respuesta => {
+                return new Promise((ok, ko) => {
+                    if (respuesta.status == 500) {
+                        return ko();
+                    }
+                    respuesta.json()
+                        .then(colores => ok(colores))
+                });
 
-           if(valido){
-
-            let[r,g,b] = valores;
-
-            return fetch("/crear-color",{
-                method: "POST",
-                body:JSON.stringify({r,g,b}),
-                headers: {
-                    "Content-type": "application/json"
-                }
             })
-            .then(respuesta => respuesta.json())
-            .then(({id,error}) =>{
-               if(!error){
-                contenedor.appendChild(li({id,r,g,b}));
-                return input.value = "";
-               }
-               parrafoError.innerText = " ha ocurrido un error";
-               parrafoError.classList.add("visible");
+            .then(colores => {
+                colores.forEach(color => contenedor.appendChild(li(color)))
+                    .catch(() => {
+                        parrafoError.innerText = "ha ocurrido un error";
+                        parrafoError.classList.add("visible");
+                    })
             });
-           }
+
+    formulario.addEventListener("submit", evento => {
+        evento.preventDefault();
+
+        parrafoError.classList.remove("visible");
+        if (/^([0-9]{1,3},){2}[0-9]{1,3}$/.test(input.value)) {
+            let valores = input.value.split(",").map(n => Number(n));
+            let valido = true;
+            let i = 0;
+
+            while (valido && i < 3) {
+                valido = valores[i] <= 255;
+                i++;
+            }
+
+            if (valido) {
+
+                let [r, g, b] = valores;
+
+                return fetch("/crear-color", {
+                    method: "POST",
+                    body: JSON.stringify({ r, g, b }),
+                    headers: {
+                        "Content-type": "application/json"
+                    }
+                })
+                    .then(respuesta => respuesta.json())
+                    .then(({ id, error }) => {
+                        if (!error) {
+                            contenedor.appendChild(li({ id, r, g, b }));
+                            return input.value = "";
+                        }
+                        parrafoError.innerText = " ha ocurrido un error";
+                        parrafoError.classList.add("visible");
+                    });
+            }
         }
 
 
--- RGB/index.js
+++ RGB/index.js
@@ -46,9 +46,30 @@ servidor.post("/crear-color", async (peticion, respuesta, siguiente) => {
 
 });
 
+servidor.delete("/borrar-color/:id(0-9)+)", async(peticion,respuesta,siguiente)=>{
+    try{
+
+        let count = await borrarColor(peticion.params.id);
+        if(count){
+            respuesta.status(500);
+            return respuesta.send("");
+        }
+        siguiente();
+        
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json({ error: " error enla servidor" });
+    }
+});
+
 servidor.use((error, peticion, respuesta, siguiente) => {
     respuesta.status(400);
     respuesta.json({ error: " error enla peticion" });
+});
+
+servidor.use((peticion,respuesta)=>{
+    respuesta.status(404);
+    respuesta.send("404 not found");
 });
 
 servidor.listen(4000);
