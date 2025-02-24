8ee795b96875e4079bd4d40932764d83ab450f82
Alejo Vera Laffratta <alejoveralaffratta21@gmail.com>
2025-02-24 08:00:24 -0300

    api todo

--- api_todo/index.js
+++ api_todo/index.js
@@ -1,36 +1,77 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
+import { leerTareas, crearTarea, borrarTarea, editarTarea, editarEstado } from "./bd";
+
 
 const servidor = express();
+servidor.use(express.json());
 
-if(process.env.PRUEBAS){
-    servidor.use("/pruebas",express.static("./pruebas"));
+if (process.env.PRUEBAS) {
+    servidor.use("/pruebas", express.static("./pruebas"));
 }
 
-servidor.get("/tareas",(peticion,respuesta)=>{
-    respuesta.send("GET /tareas");
+servidor.get("/tareas", async (peticion, respuesta) => {
+    try {
+        let tareas = await leerTareas();
+        respuesta.json(tareas);
+
+    } catch (error) {
+        respuesta.status(500);
+        respuesta.json({error :"error en el servidor"});
+    }
 });
 
-servidor.post("/tareas/nuevas",(peticion,respuesta)=>{
-    respuesta.send("POST /tareas/nueva");
+servidor.post("/tareas/nuevas", async(peticion, respuesta,siguiente) => {
+    let {tarea} = peticion.body;
+    let valido=tarea && tarea.trim() != "";
+
+    if(valido){
+        try{
+            let id = await crearTarea(tarea);
+            return respuesta.json({id});
+        }catch(error){
+            respuesta.status(500);
+            respuesta.json({error:"error en el  servidor"});
+
+        }
+    }
+
+
 });
 
-servidor.delete("/tareas/borrar/:id([0-9]+)",(peticion,respuesta)=>{
+servidor.delete("/tareas/borrar/:id([0-9]+)", async (peticion, respuesta,siguiente) => {
     respuesta.send("DELETE /tareas/borrar/:id");
+    // respuesta 204.
+    // recordad que si no puede borrar algo es 404.
+    try{
+        let count = await borrarTarea(peticion.params.id);
+        if(count){
+            respuesta.status(204);
+            return respuesta.send("");
+        }
+        siguiente();
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json({error: " error en el servidor"});
+    }
 });
 
-servidor.put("/tareas/editar/texto/:id([0-9]+)",(peticion,respuesta)=>{
-    respuesta.send("PUT /tareas/editar/texto/:id" );
+servidor.put("/tareas/editar/texto/:id([0-9]+)", (peticion, respuesta) => {
+    respuesta.send("PUT /tareas/editar/texto/:id");
 });
 
-servidor.put("/tareas/editar/estado/:id([0-9]+)",(peticion,respuesta)=>{
+servidor.put("/tareas/editar/estado/:id([0-9]+)", (peticion, respuesta) => {
     respuesta.send("PUT /tareas/editar/estado/:id");
 });
 
-servidor.use((peticion,respuesta)=>{
+servidor.use((error, peticion, respuesta, siguiente) => {
     respuesta.status(404);
-    respuesta.json({error :"recurso no encontrado"});
+    respuesta.json({ error: "recurso no encontrado" });
+});
+
+
+servidor.use((peticion, respuesta) => {
+    respuesta.status(404);
+    respuesta.json({ error: "recurso no encontrado" });
 });
 
 
